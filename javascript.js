72456474bb07d0b592ed65740353d3dd009126a3
Nicole Marty <nmarty310@gmail.com>
2020-12-23 16:49:01 -0600

    trying out how I'm going to work out the animation. models spread out if you click on the center one.

---
+++ javascript.js
@@ -0,0 +1,11 @@
+modelFive.onclick = function () {
+  modelFive.classList.add('animatedFive')
+  modelFour.classList.add('animatedFiveFour')
+  modelThree.classList.add('animatedFiveThree')
+  modelTwo.classList.add('animatedFiveTwo')
+  modelOne.classList.add('animatedFiveOne')
+  modelSix.classList.add('animatedFiveSix')
+  modelSeven.classList.add('animatedFiveSeven')
+  modelEight.classList.add('animatedFiveEight')
+  modelNine.classList.add('animatedFiveNine')
+}
